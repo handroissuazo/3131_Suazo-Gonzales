43f3403f9d154ff504aa08f54f36ca56f77f6b6e
dimasgonzalesTAMU <dimas.gonzales@tamu.edu>
2015-09-19 20:58:47 -0500

    Adding a quick and dirty implementation of insert and print so that we can see the goods

--- Part1/linked_list.c
+++ Part1/linked_list.c
@@ -1,42 +1,40 @@
-#include <stdio.h>
 #include "linked_list.h"
 
 typedef struct node
 {	//This will hold our key, value, next node, and value_length (to know how much space to allocate and deallocate for some dynamic memory allocation! Yessss... )
 	int key;
-	char* value;
+	char *value;
 	int value_length;
 	struct node *next;
 } node;
 
 typedef struct singlyLinkedList
 {
-	node *head;
-	node *tail;
+	struct node *head;
+	struct node *tail;
 	int blockSizeInBytes;
 } singlyLinkedList;
 
-//Global Variables... I know... We don't have classes here so it's a bit tricky. 
+//Global Variables... I know... We don't have classes here so it's a bit tricky.
 singlyLinkedList theList;
 
 void Init (int M, int b)
 {
-
 	// Make the head node. FYI, malloc allocates a memory block of size m and returns a pointer to the start of the alloocated block!
-	theList.head = (struct node *) malloc(M);
-	theList.head->next = 0;
+	theList.head = (struct node*) malloc(M);
+	theList.head->next = NULL;
 
 	// Make the tail node
 	theList.tail = theList.head;
 
 	// Set the default byte size
-	if (b == NULL || b == 0)
+	if (!b || b == 0)
 	{
 		// Set default to 128 bytes
 		theList.blockSizeInBytes = 128;
 	}
 	else
-	{	
+	{
 		// If given, set blockSize to b
 		theList.blockSizeInBytes = b;
 	}
@@ -44,9 +42,9 @@ void Init (int M, int b)
 
 void Destroy ()
 {	// The destroy deletes theList from the head to tail
-	node* current;
+	struct node* current;
 	while (theList.head != theList.tail)
-	{	
+	{
 		current = theList.head;
 		theList.head = theList.head->next;
 		free(current);
@@ -56,11 +54,41 @@ void Destroy ()
 	free(theList.head);
 }
 
-int Insert (int key,char * value_ptr, int value_len)
+int Insert (int key,char *value_ptr, int value_len)
 {
-		
+	struct node *newNode = (struct node*) malloc(value_len);
+	newNode->key = key;
+	newNode->value = value_ptr;
+	newNode->value_length = value_len;
+	newNode->next = NULL;
+
+	struct node *iterator = theList.head;
+	while(iterator->next != NULL)
+	{
+		iterator = iterator->next;
+	}
+
+	iterator->next = newNode;
+	// newNode->next = theList.tail;
+
 }
 
 int 	Delete (int key){}
+
 char* 	Lookup (int key){return NULL;}
-void 	PrintList (){}
+
+void 	PrintList ()
+{
+	struct node *iterator = theList.head;
+
+	//loop through all the nodes in a list except for the last
+	//node because it's next property should be null
+	//TODO: Add a is_list_empty check for this stuff
+	while (iterator->next != NULL)
+	{
+		printf("\tKey: %d, Value: %s\n", iterator->key, iterator->value);
+		iterator = iterator->next;// + iterator->value_length;
+	}
+
+	printf("\tKey: %d, Value: %s\n", iterator->key, iterator->value);
+}
--- Part1/main.c
+++ Part1/main.c
@@ -1,17 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
-int main(int argc, char ** argv) 
+#include "linked_list.h"
+
+int main(int argc, char ** argv)
 {
 	int b = 128;
 	int M = b * 11;  // so we have space for 11 items
-	
+
 	char buf [1024];
 	memset (buf, 1, 1024);		// set each byte to 1
-	
+
 	char * msg = "a sample message";
-	
+
 	Init (M,b); // initialize
 	// test operations
 	int testnums [] = {100, 5, 200, 7, 39, 25, 400, 50, 200, 300};
@@ -21,28 +20,28 @@ int main(int argc, char ** argv)
 	{
 		Insert (testnums [i], buf, 50);   // insert 50 bytes from the buffer as value for each of the insertions
 	}
-	Insert (150, buf, 200); // this Insert should fail
+	// Insert (150, buf, 200); // this Insert should fail
 	PrintList ();
-	Delete (7);
-	Insert (13, msg, strlen(msg)+1);		// insertion of strings, copies the null byte at the end
-	Delete (55);
-	Insert (15, "test msg", 8);
-	Delete (3);
-	PrintList ();
-	
+	// Delete (7);
+	// Insert (13, msg, strlen(msg)+1);		// insertion of strings, copies the null byte at the end
+	// Delete (55);
+	// Insert (15, "test msg", 8);
+	// Delete (3);
+	// PrintList ();
+
 	// a sample lookup operations that should return null, because it is looking up a non-existent number
-	char* kv = Lookup (3);
-	if (kv)
-		printf ("Key = %d, Value Len = %d\n", *(int *) kv, *(int *) (kv+4));
-	
-	
-	// this look up  should succeed and print the string "a sample message"
-	kv = Lookup (13);
-	if (kv)
-		printf ("Key = %d, Value Len = %d, Value = %s\n", *(int *) kv, *(int *) (kv+4), kv + 8);
-	
-	
-	// end test operations	
+	// char* kv = Lookup (3);
+	// if (kv)
+	// 	printf ("Key = %d, Value Len = %d\n", *(int *) kv, *(int *) (kv+4));
+
+
+	// this look up should succeed and print the string "a sample message"
+	// kv = Lookup (13);
+	// if (kv)
+	// 	printf ("Key = %d, Value Len = %d, Value = %s\n", *(int *) kv, *(int *) (kv+4), kv + 8);
+
+
+	// end test operations
 	Destroy ();
-	
+
 }
